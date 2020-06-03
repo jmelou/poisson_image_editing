e6300af329ff95d6eb8cfbf00330863b78446534
jmelou <jeme@mikrosimage.eu>
2020-06-03 19:20:50 +0200

    Read and diplay picture

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world !")
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read source image :
+source = np.array(Image.open('Pictures/orca.jpg'))
+nb_row_s,nb_col_s,nb_ch_s = source.shape
+
+plt.imshow(s)
+plt.show()
